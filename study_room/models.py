5a82af38e1737aec5ddc74cdab32c1fdaf38527d
panitnt <panittatanya@gmail.com>
2022-08-05 13:47:16 +0700

    create study_room models

--- study_room/models.py
+++ study_room/models.py
@@ -1,3 +1,16 @@
+from re import sub
+from time import time
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    subject = models.CharField(max_length=255)
+    by = models.CharField(max_length=255)
+    university = models.CharField(max_length=255, blank=True)
+    date = models.DateField()
+    time = models.TimeField()
+    people = models.IntegerField()
+    url = models.URLField()
+
+    def __str__(self) -> str:
+        return f'{self.subject} by {self.by} : {self.university} {self.date} {self.time}'
