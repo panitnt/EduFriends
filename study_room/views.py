d884487dad0062ee69fe572ee5fc8c2b648430c1
panitnt <panittatanya@gmail.com>
2022-08-08 21:44:31 +0700

    edit study_room html

--- study_room/views.py
+++ study_room/views.py
@@ -4,5 +4,5 @@ from .models import Room
 # Create your views here.
 def study_room(request):
     room = Room.objects.all()
-    return render(request, 'study_room/study_room.html', {'room': room})
+    return render(request, 'study_room/study_room.html', {'rooms': room})
 
